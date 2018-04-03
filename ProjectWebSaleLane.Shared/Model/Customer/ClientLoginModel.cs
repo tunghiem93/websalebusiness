951c12fa583246a44fd7cd18ff16ad3ddbaf222a
TuanVo <voquoctuanqn@gmail.com>
2018-04-03 18:05:25 +0700

    [+] add file model clientlogin

---
+++ ProjectWebSaleLane.Shared/Model/Customer/ClientLoginModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWebSaleLane.Shared.Model.Customer
+{
+    public class ClientLoginModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập tên đăng nhập")]
+       // [Display(Name = "Vui lòng nhập Username")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+      //  [Display(Name = "Vui lòng nhập mật khẩu")]
+        public string Password { get; set; }
+        public bool IsRemember { get; set; }
+        public string DisplayName { get; set; }
+
+        public ClientLoginModel()
+        {
+            IsRemember = true;
+        }
+    }
+}
