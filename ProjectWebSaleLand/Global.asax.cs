a8d1a7725ef00d8dd1baf27d4a5379e134b748d3
TuNghiem93 <tunx406.ktl@gmail.com>
2018-04-05 10:39:47 +0700

    [M] Fix session

--- ProjectWebSaleLand/Global.asax.cs
+++ ProjectWebSaleLand/Global.asax.cs
@@ -43,7 +43,7 @@ namespace ProjectWebSaleLand
             {
                 var input = Server.UrlDecode(_UserClientCookie.Value);
                 UserSession userSession = JsonConvert.DeserializeObject<UserSession>(input); //new JavaScriptSerializer().Deserialize<UserSession>(input);
-                if (userSession != null)
+                if (userSession != null && HttpContext.Current.Session != null)
                 {
                     Session.Add("UserClient", userSession);
                 }
